177feaa11ef94d579a854755351fd67df56f31af
telebotmake <lgbtqopo@gmail.com>
2025-04-04 19:58:29 +0330

    Update BroadcastMod.java

--- src/main/java/yourpackage/BroadcastMod.java
+++ src/main/java/yourpackage/BroadcastMod.java
@@ -1,39 +1,33 @@
 package yourpackage;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.text.Text;
 
-import com.mojang.brigadier.context.CommandContext;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
-public class BroadcastMod implements ModInitializer {
+public class BroadcastModClient implements ClientModInitializer {
 
     @Override
-    public void onInitialize() {
-        System.out.println("[Broadcast Mod] مود فعال شد!");
+    public void onInitializeClient() {
+        ClientCommandManager.DISPATCHER.register(
+            literal("broadcast-ip").executes(context -> {
+                MinecraftClient client = MinecraftClient.getInstance();
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(CommandManager.literal("broadcast-ip")
-                .executes(context -> broadcastIP(context, "بیا 5.10.248.159")));
-        });
-    }
+                if (client.getNetworkHandler() == null || client.player == null) {
+                    return 0;
+                }
 
-    private int broadcastIP(CommandContext<ServerCommandSource> context, String message) {
-        MinecraftServer server = context.getSource().getServer();
+                for (PlayerListEntry entry : client.getNetworkHandler().getPlayerList()) {
+                    String playerName = entry.getProfile().getName();
+                    client.player.sendChatMessage("/msg " + playerName + " بیا 5.10.248.159");
+                }
 
-        for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-            String playerName = player.getName().getString();
-            String command = String.format("msg %s %s", playerName, message);
-            server.getCommandManager().executeWithPrefix(context.getSource(), command);
-        }
-
-        // اصلاح شده با Supplier<Text>
-        context.getSource().sendFeedback(() -> Text.literal("پیام \"" + message + "\" به همه ارسال شد!"), false);
-
-        return 1;
+                client.inGameHud.getChatHud().addMessage(Text.literal("پیام برای همه پلیرها فرستاده شد."));
+                return 1;
+            })
+        );
     }
 }
